b7a3f7d02b0ea3f1e5534f301f33ca3c1a31952b
carlos <carlosasjr2003@hotmail.com>
2022-12-06 21:22:34 -0300

    teste axios 2

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,11 +1,11 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
-axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
-axios.defaults.headers['Access-Control-Allow-Origin'] = '*'
-
 const api = axios.create({ baseURL: process.env.API })
 api.defaults.withCredentials = true
+
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+axios.defaults.headers['Access-Control-Allow-Origin'] = '*'
 
 const axiosOpenCharge = axios.create({ baseURL: 'https://api.openchargemap.io/v3/poi/' })
 
